7c7f59191334a387246f955a0b5f194d3c20c333
Isaiah Rohrer <isaiah.rohrer@gmail.com>
2025-10-15 19:42:23 -0400

    removed debug couts

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ pair<int,int> diagonals[] = {{-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
 pair<int,int> dirs[] = {{0,1},{1,0},{0,-1},{-1,0}};
 
 
-void findValidPosition(vector<vector<char>> &maze, int &x, int &y);
+bool findValidPosition(vector<vector<char>> &maze, int &x, int &y);
 bool validPosition(vector<vector<char>> &maze, int x, int y);
 bool validCand(vector<vector<char>> &maze,vector<pair<int,int>> &path, pair<int,int> p);
 
@@ -35,31 +35,24 @@ int main(){
     int x = getRandom(rows), y = getRandom(cols);
     findValidPosition(maze, x, y);
     maze[x][y] = ' ';
+
     x = getRandom(rows); y = getRandom(cols);
-
-
-    while (x != -1 && y != -1){
-        // cout << "Starting new path" << x << y<< endl;
-
+    while (findValidPosition(maze, x, y)){
         printMaze(maze);
-        x = getRandom(rows);
-        y = getRandom(cols);
-        // cout << "x " << x << " y " << y << endl;
-        findValidPosition(maze, x, y);
-        // cout << "\t"<< x << " "<< y<< endl;
 
         genPath(maze, x, y);
+
+        x = getRandom(rows);
+        y = getRandom(cols);
+
         printMaze(maze);
     }
     return 1;
 }
-void findValidPosition(vector<vector<char>> &maze, int &x, int &y){
+bool findValidPosition(vector<vector<char>> &maze, int &x, int &y){
     if( x >= rows || y >= cols || x < 0 || y < 0) {
-        x = -1;
-        y = -1;
-        return;
+        return 0;
     }
-    // cout << "1" << endl;
     int start;
     for (int i = x; i < rows; i++){
         if (i == x) start = y;
@@ -68,45 +61,32 @@ void findValidPosition(vector<vector<char>> &maze, int &x, int &y){
             if (validPosition(maze, i, j)) {
                 x = i;
                 y = j;
-                return;
+                return 1;
             }
         }
     }
-    // cout << "2" << endl;
 
     for (int i = 0; i <= x; i++){
         for (int j = 0; j < cols; j++){
             if (validPosition(maze, i, j)) {
                 x = i;
                 y = j;
-                return;
+                return 1;
             }
         }
     }
-    // cout << "3" << endl;
-
-    x = -1;
-    y = -1;
-    return;
+    return 0;
 }
 
 void genPath(vector<vector<char>> &maze, int x, int y) {
     if (x < 0 || x >= rows || y < 0 || y >= cols) return;
 
-    vector<pair<int,int>> path;          // acts as stack
+    vector<pair<int,int>> path;          
     path.push_back({x,y});
     maze[x][y] = '+';
 
-
-
     while (!path.empty()) {
         auto [cx, cy] = path.back();
-        // cout << "\nCurrent top of path: (" << cx << "," << cy << ") pos: " << maze[cx][cy] << "\n";
-        // cout << "Rest of path:\n";
-       // for(auto p : path){
-            // cout << "{" << p.first << ", " << p.second << "}\n";
-      //  }
-        // Collect valid moves
         vector<pair<int,int>> candidates;
         vector<pair<int,int>> prev;
         
@@ -133,25 +113,19 @@ void genPath(vector<vector<char>> &maze, int x, int y) {
         pair<int,int> target = {-1,-1};
         pair<int,int> temp = {-1,-1};
         if(prev.size() > 1){
-            // cout << "Current place is not valid" << endl;
             while(!prev.empty()){ 
                 
                 auto it = find(path.begin(),path.end(), prev.back());
-                //if(it == path.end()) cout << "\nnot found\n";
                 temp = *it;
-                // cout << temp.first << " " << temp.second << endl;
                 prev.pop_back();
                 if (oldest > it) oldest = it;
                 target = *oldest;
             }
-            // cout << endl;
-            // cout << target.first << " " << target.second << endl;
+
             while (path.back() != target){
-                // cout << path.size() << endl;
                 maze[path.back().first][path.back().second] = '#';
                 path.pop_back();
             }
-            // cout << path.size() << endl;
 
         }
         else if(candidates.empty()){
@@ -161,7 +135,6 @@ void genPath(vector<vector<char>> &maze, int x, int y) {
             }
         }
         else {
-            // Pick a random candidate
             auto [nx, ny] = candidates[rand() % candidates.size()];
             maze[nx][ny] = '+';
             path.push_back({nx, ny});
@@ -170,25 +143,16 @@ void genPath(vector<vector<char>> &maze, int x, int y) {
         printMaze(maze);
     }
 
-    // Solidify the path
     for (int i = 0; i < rows; i++)
         for (int j = 0; j < cols; j++)
             if (maze[i][j] == '+') maze[i][j] = ' ';
 }
-
-
-
-
-
-
-
 
 int getRandom(int limit){
     return rand() % limit;
 }
 
 bool validPosition(vector<vector<char>> &maze, int x, int y){
-    // cout << "Checking: {" << x << ", "<< y << "}" << endl; 
     if( x >= rows || y >= cols || x < 0 || y < 0) return 0;
     if(maze[x][y] != '#') return 0;
     int path_neighbors = 0;
@@ -216,7 +180,6 @@ bool validPosition(vector<vector<char>> &maze, int x, int y){
 bool validCand(vector<vector<char>> &maze, vector<pair<int,int>> &path, pair<int,int> p){
     int dx, dy, count = 0;
     if (maze[p.first][p.second] == '#' && find(path.begin(), path.end(), p) == path.end()) {
-        // 
         for (auto dia : diagonals) {
             dx = dia.first + p.first;
             dy = dia.second + p.second;
@@ -226,15 +189,13 @@ bool validCand(vector<vector<char>> &maze, vector<pair<int,int>> &path, pair<int
             }
         }
         if (count > 1) return false;
-        //
         return true;
     }
     return false;
 }
 
 void printMaze(vector<vector<char>> &maze){
-    // cout << "\n\n";
-    // cout << rows << " " << cols;
+
     cout << '\n';
     for (int i = 0; i < cols + 2; i++) cout << "#";
     cout << '\n';
